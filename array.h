9ed880068082d4e2816530280deeb55daa2e8839
Kacper <250946@edu.p.lodz.pl>
2024-05-23 19:20:13 +0200

    add dynamic

--- array.h
+++ array.h
@@ -4,5 +4,11 @@
 
 #ifndef UNTITLED5_ARRAY_H
 #define UNTITLED5_ARRAY_H
+int findMaximumValue(int array[], int SIZE);
+int findMiniumValue(int array[], int SIZE);
+float calculateAverage(int array[], int SIZE);
+int* enterValue(int *array, int *s);
+void printArray(int array[], int s);
+int* deleteValues(int *array, int *s);
 
 #endif //UNTITLED5_ARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "array.h"
+void menu() {
+    printf("1- Intput Values\n");
+    printf("2- Delete Values\n");
+    printf("3- Print Array\n");
+    printf("4- Find Maximum Value\n");
+    printf("5- Find Minium Value\n");
+    printf("6- Calculate Average\n");
+    printf("7- EXIT Program\n");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int *array = NULL;
+    int option = 0;
+    int SIZE = 0;
+    while(option!=9) {
+        menu();
+        printf("choice = ");
+        scanf("%d", &option);
+        switch(option){
+            case 1 :
+                array = enterValue(array, &SIZE);
+                break;
+            case 2 :
+                array = deleteValues(array,&SIZE);
+                break;
+            case 3 :
+                printArray(array, SIZE);
+                break;
+            case 4 :
+                printf("maxmium value = %d\n",findMaximumValue(array,SIZE));
+                break;
+            case 5 :
+                printf("minium value = %d\n",findMiniumValue(array,SIZE));
+                break;
+            case 6:
+                printf("Average valute = %f\n", calculateAverage(array,SIZE));
+                break;
+            case 7 :
+                break;
+
+
+
+        }
+    }
+    free(array);
 }
